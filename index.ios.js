2f793f5c4380c14bddcfd567e38e31244ccdcf89
Ilter Canberk <ilter@skurt.com>
2017-08-24 16:00:08 -0700

    Use custom models & add text

--- index.ios.js
+++ index.ios.js
@@ -12,20 +12,68 @@ import {
   View
 } from 'react-native';
 
+import ARScene, { ARBoxNode, ARTextNode, ARNode } from 'react-arkit';
+
 export default class ReactArkitExample extends Component {
-  render() {
+
+  constructor (props) {
+    super(props);
+    this.state = { planes: {} };
+    this.newPlaneDetected = this.newPlaneDetected.bind(this);
+    this.planedUpdated = this.planedUpdated.bind(this);
+  }
+
+  newPlaneDetected ({id, alignment, node, center, extent}) {
+    let { planes } = this.state;
+    planes[id] = { id, node, center };
+    this.setState({ planes });
+  }
+
+  planedUpdated ({id, alignment, node, center, extent}) {
+    let { planes } = this.state;
+    planes[id] = { id, node, center };
+    this.setState({ planes });
+  }
+
+  renderBoxes () {
+    return Object.values(this.state.planes).map((plane) => {
+
+        return plane.node.y < -0.5
+          ? <ARNode
+              key={plane.id}
+              size={{ scale: 0.01 }}
+              modelAssetPath={'CustomModels.scnassets/flower.dae:vijverplant'}
+              geoposition={{x: plane.node.x,
+                            y: plane.node.y + 0.05,
+                            z: plane.node.z}}/>
+          : <ARNode
+              key={plane.id}
+              size={{ scale: 0.02 }}
+              modelAssetPath={'CustomModels.scnassets/BarsikCat.dae:cat'}
+              geoposition={{x: plane.node.x,
+                            y: plane.node.y + 0.07,
+                            z: plane.node.z}}/>
+              });
+  }
+
+
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <ARScene style={styles.scene}
+            debugEnabled
+            onPlaneDetected={this.newPlaneDetected}
+            onPlaneUpdated={this.planedUpdated}>
+          
+          <ARTextNode
+              text={'Look at the floor'}
+              color='#FF0'
+              size={{ fontSize: 0.15, depth: 0.1 }}
+              geoposition={{x: 0, y: 0.01, z:-1.5}}/>
+
+          { this.renderBoxes() }
+
+        </ARScene>
       </View>
     );
   }
@@ -34,20 +82,12 @@ export default class ReactArkitExample extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  scene: {
+    flex: 1,
+    backgroundColor: '#00FF00'
+  }
 });
 
 AppRegistry.registerComponent('ReactArkitExample', () => ReactArkitExample);
